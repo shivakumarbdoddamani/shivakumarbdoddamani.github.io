7a5f7081fb9630b273ef2b08dd67ccfc71ea4284
Shivakumar <13shivu13@gmail.com>
2025-01-08 21:41:02 +0530

    build issue fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        // Ensure proper file extensions and formats
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
