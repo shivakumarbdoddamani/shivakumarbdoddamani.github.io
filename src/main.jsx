c27d45b15fed91f9b0f43a85e1e6d1c03f8edaf5
Shivakumar <13shivu13@gmail.com>
2025-01-08 22:03:35 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import reportWebVitals from "./reportWebVitals";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
